1495e6b236481b4f57a200d8cadcefcd02774e77
Pranish Shrestha <pranishshrestha181@gmail.com>
2022-07-08 13:03:09 +0545

    feat: works like a charm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SignUp from "./components/SignUp";
+import "./app.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container-fluid mt-3 text-center">
+        <p>form validation using formik and Yup</p>
+        <div className="row d-flex justify-content-center">
+          <div className="col-sm-5 bg-slate-white px-2 py-4 rounded text-black">
+            <SignUp />
+          </div>
+         
+        </div>
+      </div>
+    </>
   );
 }
 
